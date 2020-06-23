c5b6143688789bd7113ff85027585188af7269e4
Branislav Kljaic <branislavkljaic@gmail.com>
2020-06-23 12:22:01 +0200

    Add Login page

---
+++ src/eduProjectWebGUI/Pages/Login.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eduProjectWebGUI.Pages
+{
+    public partial class Login
+    {
+        private void LoginToSystem(MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
