601346e2d1c04d2af10fc461d8c9717d6cc03648
Branislav Kljaic <branislavkljaic@gmail.com>
2020-06-29 13:58:46 +0200

    Add view project page with generic data.

---
+++ src/eduProjectWebGUI/Pages/ProjectView.razor.cs
@@ -0,0 +1,16 @@
+﻿using eduProjectWebGUI.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eduProjectWebGUI.Pages
+{
+    public partial class ProjectView
+    {
+        void ShowModal()
+        {
+            Modal.Show<LeaveComment>();
+        }
+    }
+}
--- src/eduProjectWebGUI/Program.cs
+++ src/eduProjectWebGUI/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazored.Modal;
 
 namespace eduProjectWebGUI
 {
@@ -18,6 +19,8 @@ namespace eduProjectWebGUI
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddBlazoredModal();
 
             await builder.Build().RunAsync();
         }
