2bd28c184fdca26d1c8078a434fdf5b4b5447c8f
Nikolina Maksimović <ninamaksimovic96@outlook.com>
2020-10-03 19:58:11 +0200

    Add Model and razor component

---
+++ src/eduProjectModelLib/Display/ApplicationsReceivedDisplayModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eduProjectModel.Display
+{
+    public class ApplicationsReceivedDisplayModel
+    {
+            public string Title { get; set; }
+            public ICollection<StudentProfileDisplayModel> StudentProfileDisplayModels { get; set; } = new HashSet<StudentProfileDisplayModel>();
+            public ICollection<FacultyMemberProfileDisplayModel> FacultyMemberProfileDisplayModels { get; set; } = new HashSet<FacultyMemberProfileDisplayModel>();
+            public ICollection<CollaboratorDisplayModel> CollaboratorDisplayModels { get; set; } = null;
+    }
+}
---
+++ src/eduProjectWebGUI/Pages/ApplicationsReceived.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eduProjectModel.Display;
+
+namespace eduProjectWebGUI.Pages
+{
+    public partial class ApplicationsReceived
+    {
+        [Parameter]
+        public ApplicationsReceivedDisplayModel ApplicationsReceivedDisplayModel { get; set; }
+
+
+        public void Close()
+        {
+
+        }
+    }
+}
