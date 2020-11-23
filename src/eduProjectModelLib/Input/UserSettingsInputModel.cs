854ca0c8c4cf845789faba327e2b64d12041941c
Nina <ninagrahovac.96@gmail.com>
2020-11-23 14:37:24 +0100

    Add user settings page

--- src/eduProjectModelLib/Input/UserSettingsInputModel.cs
+++ src/eduProjectModelLib/Input/UserSettingsInputModel.cs
@@ -1,4 +1,5 @@
-﻿using eduProjectModel.Domain;
+﻿using eduProjectModel.Display;
+using eduProjectModel.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,18 +21,38 @@ namespace eduProjectModel.Input
         public string Bio { get; set; }
         public ICollection<string> UserTagNames { get; set; } = new HashSet<string>();
 
-        public void MapTo(UserSettings settings)
+        public UserSettingsInputModel()
         {
-            settings.UserId = UserId;
-            settings.EmailVisible = EmailVisible;
-            settings.PhoneVisible = PhoneVisible;
-            settings.ProjectsVisible = ProjectsVisible;
-            settings.LinkedinProfile = LinkedinProfile;
-            settings.ResearchgateProfile = ResearchgateProfile;
-            settings.Website = Website;
-            settings.CvLink = CvLink;
-            settings.PhotoLink = PhotoLink;
-            settings.Bio = Bio;
+
+        }
+
+        public UserSettingsInputModel(UserSettingsDisplayModel model)
+        {
+            UserId = model.UserId;
+            EmailVisible = model.EmailVisible;
+            PhoneVisible = model.PhoneVisible;
+            ProjectsVisible = model.ProjectsVisible;
+            LinkedinProfile = model.LinkedinProfile;
+            ResearchgateProfile = model.ResearchgateProfile;
+            Website = model.Website;
+            CvLink = model.CvLink;
+            PhotoLink = model.PhotoLink;
+            Bio = model.Bio;
+            UserTagNames = model.UserTags.Select(t => t.Name).ToList();
+        }
+
+        public void MapTo(UserSettings model)
+        {
+            model.UserId = UserId;
+            model.EmailVisible = EmailVisible;
+            model.PhoneVisible = PhoneVisible;
+            model.ProjectsVisible = ProjectsVisible;
+            model.LinkedinProfile = LinkedinProfile;
+            model.ResearchgateProfile = ResearchgateProfile;
+            model.Website = Website;
+            model.CvLink = CvLink;
+            model.PhotoLink = PhotoLink;
+            model.Bio = Bio;
 
             HashSet<Tag> tags = new HashSet<Tag>();
             foreach (var name in UserTagNames)
@@ -39,7 +60,9 @@ namespace eduProjectModel.Input
                 tags.Add(Tag.tags.Where(i => i.Value.Name == name).First().Value);
             }
 
-            settings.UserTags = tags;
+            model.UserTags = tags;
         }
+
+
     }
 }
