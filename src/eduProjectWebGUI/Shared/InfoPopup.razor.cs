6540d815da961e3847b0581c72e5f78f51e23032
Nina <ninagrahovac.96@gmail.com>
2021-02-02 21:01:36 +0100

    Add info popup for displaying result of user action

---
+++ src/eduProjectWebGUI/Shared/InfoPopup.razor.cs
@@ -0,0 +1,23 @@
+﻿using Blazored.Modal;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eduProjectWebGUI.Shared
+{
+    public partial class InfoPopup
+    {
+        [CascadingParameter]
+        BlazoredModalInstance BlazoredModal { get; set; }
+
+        [Parameter]
+        public string Message { get; set; }
+
+        void Ok()
+        {
+            BlazoredModal.Close();
+        }
+    }
+}
