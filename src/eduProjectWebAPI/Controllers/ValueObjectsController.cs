c76f069b069a5aedbeeeda9f6a22bc0a8a990ba3
Nina <ninagrahovac.96@gmail.com>
2021-03-26 09:17:38 +0100

    Remove auth checks from ValueObjectsController for registration

--- src/eduProjectWebAPI/Controllers/ValueObjectsController.cs
+++ src/eduProjectWebAPI/Controllers/ValueObjectsController.cs
@@ -25,87 +25,89 @@ namespace eduProjectWebAPI.Controllers
         [HttpGet("/tags")]
         public async Task<ActionResult<Dictionary<string, Tag>>> GetTags()
         {
+            /*
             if (HttpContext.Request.ExtractUserId() == null)
             {
                 return Unauthorized();
             }
             else
+            {*/
+            Dictionary<string, Tag> tags = new Dictionary<string, Tag>();
+
+            using (var connection = new MySqlConnection(dbConnectionString.ConnectionString))
             {
-                Dictionary<string, Tag> tags = new Dictionary<string, Tag>();
-
-                using (var connection = new MySqlConnection(dbConnectionString.ConnectionString))
+                MySqlCommand command = new MySqlCommand
                 {
-                    MySqlCommand command = new MySqlCommand
-                    {
-                        Connection = connection,
-                        CommandText = @"SELECT tag_id, name, description FROM tag"
-                    };
+                    Connection = connection,
+                    CommandText = @"SELECT tag_id, name, description FROM tag"
+                };
 
-                    await connection.OpenAsync();
+                await connection.OpenAsync();
 
-                    using (var reader = await command.ExecuteReaderAsync())
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    if (reader.HasRows)
                     {
-                        if (reader.HasRows)
+                        while (await reader.ReadAsync())
                         {
-                            while (await reader.ReadAsync())
+                            tags.Add($"{reader.GetInt32(0)}", new Tag
                             {
-                                tags.Add($"{reader.GetInt32(0)}", new Tag
-                                {
-                                    Name = reader.GetString(1),
-                                    Description = !reader.IsDBNull(2) ? reader.GetString(2) : null
-                                });
-                            }
+                                Name = reader.GetString(1),
+                                Description = !reader.IsDBNull(2) ? reader.GetString(2) : null
+                            });
                         }
                     }
-
-                    await connection.CloseAsync();
                 }
 
-                return tags;
+                await connection.CloseAsync();
             }
+
+            return tags;
+            //}
         }
 
         [HttpGet("/fields")]
         public async Task<ActionResult<Dictionary<string, StudyField>>> GetStudyFields()
         {
+            /*
             if (HttpContext.Request.ExtractUserId() == null)
             {
                 return Unauthorized();
             }
             else
+            {*/
+            Dictionary<string, StudyField> studyFields = new Dictionary<string, StudyField>();
+
+            using (var connection = new MySqlConnection(dbConnectionString.ConnectionString))
             {
-                Dictionary<string, StudyField> studyFields = new Dictionary<string, StudyField>();
-
-                using (var connection = new MySqlConnection(dbConnectionString.ConnectionString))
+                MySqlCommand command = new MySqlCommand
                 {
-                    MySqlCommand command = new MySqlCommand
-                    {
-                        Connection = connection,
-                        CommandText = @"SELECT study_field_id, name, description FROM study_field"
-                    };
+                    Connection = connection,
+                    CommandText = @"SELECT study_field_id, name, description FROM study_field"
+                };
 
-                    await connection.OpenAsync();
+                await connection.OpenAsync();
 
-                    using (var reader = await command.ExecuteReaderAsync())
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    if (reader.HasRows)
                     {
-                        if (reader.HasRows)
+                        while (await reader.ReadAsync())
                         {
-                            while (await reader.ReadAsync())
+                            studyFields.Add($"{reader.GetInt32(0)}", new StudyField
                             {
-                                studyFields.Add($"{reader.GetInt32(0)}", new StudyField
-                                {
-                                    Name = reader.GetString(1),
-                                    Description = !reader.IsDBNull(2) ? reader.GetString(2) : null
-                                });
-                            }
+                                Name = reader.GetString(1),
+                                Description = !reader.IsDBNull(2) ? reader.GetString(2) : null
+                            });
                         }
                     }
-
-                    await connection.CloseAsync();
                 }
 
-                return studyFields;
+                await connection.CloseAsync();
             }
+
+            return studyFields;
+            //}
         }
 
 
