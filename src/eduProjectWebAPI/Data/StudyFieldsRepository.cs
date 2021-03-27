92d5395a7bc242095f9b93799f2a0bc0d3ace38c
Nina <ninagrahovac.96@gmail.com>
2021-03-27 13:20:20 +0100

    Implement repository method for adding new study field.

--- src/eduProjectWebAPI/Data/StudyFieldsRepository.cs
+++ src/eduProjectWebAPI/Data/StudyFieldsRepository.cs
@@ -1,6 +1,8 @@
 ﻿using eduProjectModel.Domain;
+using MySqlConnector;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +10,42 @@ namespace eduProjectWebAPI.Data
 {
     public class StudyFieldsRepository : IStudyFieldsRepository
     {
-        public Task AddAsync(StudyField field)
+        private readonly DbConnectionParameters dbConnectionParameters;
+        public StudyFieldsRepository(DbConnectionParameters dbConnectionParameters)
         {
-            throw new NotImplementedException();
+            this.dbConnectionParameters = dbConnectionParameters;
+        }
+        public async Task AddAsync(StudyField field)
+        {
+            using (var connection = new MySqlConnection(dbConnectionParameters.ConnectionString))
+            {
+                var command = new MySqlCommand
+                {
+                    Connection = connection,
+                    CommandText = @"INSERT INTO study_field
+                                    (name, description)
+                                    VALUES
+                                    (@name, @description)"
+                };
+
+                command.Parameters.Add(new MySqlParameter
+                {
+                    ParameterName = "@name",
+                    DbType = DbType.String,
+                    Value = field.Name
+                });
+
+                command.Parameters.Add(new MySqlParameter
+                {
+                    ParameterName = "@description",
+                    DbType = DbType.String,
+                    Value = field.Description
+                });
+
+                await connection.OpenAsync();
+                await command.ExecuteNonQueryAsync();
+                await connection.CloseAsync();
+            }
         }
     }
 }
