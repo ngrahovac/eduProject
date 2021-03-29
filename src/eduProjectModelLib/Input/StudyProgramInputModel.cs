7a2f8ecc1a42c5ad3eebbe7570bbc9152f96d9e7
Nina <ninagrahovac.96@gmail.com>
2021-03-29 20:24:38 +0200

    Update StudyProgramInputModel. Add basic layout for administering faculties.

--- src/eduProjectModelLib/Input/StudyProgramInputModel.cs
+++ src/eduProjectModelLib/Input/StudyProgramInputModel.cs
@@ -5,10 +5,18 @@ namespace eduProjectModel.Input
 {
     public class StudyProgramInputModel
     {
+        public string Name { get; set; }
+        public int Cycle { get; set; }
+        public int DurationYears { get; set; }
+
         public ICollection<StudyProgramSpecializationInputModel> StudyProgramSpecializationInputModels { get; set; } = new List<StudyProgramSpecializationInputModel>();
 
         public void MapTo(StudyProgram studyProgram)
         {
+            studyProgram.Name = Name;
+            studyProgram.Cycle = (byte)Cycle;
+            studyProgram.DurationYears = (byte)DurationYears;
+
             foreach (var model in StudyProgramSpecializationInputModels)
             {
                 StudyProgramSpecialization specialization = new StudyProgramSpecialization();
