112234f3631e4f4d83c048574f0b86ec7e675d8a
Zoran Pantos <pantos78zoran@gmail.com>
2021-04-10 20:48:48 +0200

    Validate adding StudyField

--- src/eduProjectModelLib/Input/StudyFieldInputModel.cs
+++ src/eduProjectModelLib/Input/StudyFieldInputModel.cs
@@ -1,10 +1,13 @@
 ﻿using eduProjectModel.Domain;
+using System.ComponentModel.DataAnnotations;
 
 namespace eduProjectModel.Input
 {
     public class StudyFieldInputModel
     {
+        [Required(ErrorMessage = "Naziv je obavezan.")]
         public string Name { get; set; }
+
         public string Description { get; set; }
 
         public void MapTo(StudyField field)
