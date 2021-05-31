29f3e0e824146fef01516889c40624aa2fd23f89
Nina <ninagrahovac.96@gmail.com>
2021-05-31 09:22:12 +0200

    Rename LoginView to LoginPage. Add redirect based on user role.

---
+++ src/eduProjectWebGUI/Pages/LoginPage.razor.cs
@@ -0,0 +1,44 @@
+﻿using eduProjectModel.Input;
+using eduProjectWebGUI.Utils;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace eduProjectWebGUI.Pages
+{
+    public partial class LoginPage
+    {
+        LoginInputModel loginInputModel = new LoginInputModel();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var token = await LocalStorage.GetItemAsync<string>("authToken");
+            if (token != null)
+            {
+                var claims = ExtensionMethods.ParseClaimsFromJwt(token);
+                if (claims.First(c => c.Type == ClaimTypes.Role).Value == "Admin")
+                    Navigation.NavigateTo("/admin/accounts");
+                else
+                    Navigation.NavigateTo("/news");
+            }
+        }
+
+        public async Task Login2()
+        {
+            var result = await AuthService.Login(loginInputModel);
+
+            if (result.Successful)
+            {
+                var token = result.Token;
+                var claims = ExtensionMethods.ParseClaimsFromJwt(token);
+                if (claims.First(c => c.Type == ClaimTypes.Role).Value == "Admin")
+                    Navigation.NavigateTo("/admin/accounts");
+                else
+                    Navigation.NavigateTo("/news");
+            }
+            else
+                Console.WriteLine(result.Error);
+        }
+    }
+}
