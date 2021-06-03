f625b2268a68d8b120ad3da2663dd17144d24605
Nina <ninagrahovac.96@gmail.com>
2021-06-03 18:31:10 +0200

    Update model validation and form for adding new faculty.

--- src/eduProjectModelLib/Input/FacultyInputModel.cs
+++ src/eduProjectModelLib/Input/FacultyInputModel.cs
@@ -1,12 +1,18 @@
 ﻿using eduProjectModel.Domain;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace eduProjectModel.Input
 {
     public class FacultyInputModel
     {
+        [Required(ErrorMessage = "Naziv fakulteta je obavezan.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Adresa fakulteta je obavezna.")]
         public string Address { get; set; }
+
+        [MinLength(1, ErrorMessage = "Neophodno je unijeti bar jedan studijski program.")]
         public ICollection<StudyProgramInputModel> StudyProgramInputModels { get; set; } = new List<StudyProgramInputModel>();
 
         public void MapTo(Faculty faculty)
